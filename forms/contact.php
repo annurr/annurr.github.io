1c9e83961b940f7746e76981f88a902ddcbd3d78
Fahim Annur Rahman <annurece@gmail.com>
2023-01-21 13:24:57 +0600

    Contact mailing Updated

--- forms/contact.php
+++ forms/contact.php
@@ -1,18 +1,12 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  
+  $receiving_email_address = 'annurece@gmail.com';
+  $php_email_form = '../assets/vendor/php-email-form/php-email-form.php'
+  if( file_exists($php_email_form)) {
     include( $php_email_form );
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    die( 'Coming Soon!!');
   }
 
   $contact = new PHP_Email_Form;
