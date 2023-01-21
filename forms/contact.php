6259fe39d8c386a25450900ecd388f6e0ecd6e20
Fahim Annur Rahman <annurece@gmail.com>
2023-01-21 18:21:59 +0600

    Added CV section and clearing garbage

--- forms/contact.php
+++ forms/contact.php
@@ -1,21 +1,23 @@
 <?php
 
   
-  $receiving_email_address = 'annurece@gmail.com';
-  $php_email_form = '../assets/vendor/php-email-form/php-email-form.php'
-  if( file_exists($php_email_form)) {
-    include( $php_email_form );
-  } else {
-    die( 'Coming Soon!!');
-  }
+  // $receiving_email_address = 'annurece@gmail.com';
+  // $php_email_form = '../assets/vendor/php-email-form/php-email-form.php'
+  // if( file_exists($php_email_form)) {
+  //   include( $php_email_form );
+  // } else {
+  //   die( 'Coming Soon!!');
+  // }
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+  echo( 'Coming Soon!!');
+
+  // $contact = new PHP_Email_Form;
+  // $contact->ajax = true;
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  // $contact->to = $receiving_email_address;
+  // $contact->from_name = $_POST['name'];
+  // $contact->from_email = $_POST['email'];
+  // $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -27,9 +29,9 @@
   );
   */
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  // $contact->add_message( $_POST['name'], 'From');
+  // $contact->add_message( $_POST['email'], 'Email');
+  // $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+  // echo $contact->send();
 ?>
